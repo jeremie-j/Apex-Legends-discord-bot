4135371fd530ca7aea173ba112ca67a9f236b6dd
Jérémie <jeremie.jourda202@gmail.com>
2021-04-16 01:48:31 +0200

    Initial commit, la base du bot, ajout de la commande stats

---
+++ bot.py
@@ -0,0 +1,28 @@
+import os
+import librairies
+from discord.ext import commands
+from commands.request import *
+
+librairies.dotenv.load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = librairies.discord.Client()
+bot = commands.Bot(command_prefix=';')
+
+
+Request=Request()
+@ bot.event
+async def on_ready():
+    print('bot online')
+    await bot.change_presence(activity=discord.Game(name="Type ;help"))
+
+@ bot.command()
+async def stats(ctx, *arg):
+    embed=await Request.getStats(arg)
+    await ctx.send(embed = embed)
+
+@ bot.command()
+async def coucou(ctx, *arg):
+    await ctx.send("non")
+
+bot.run(TOKEN)
